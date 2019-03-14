a343a5d509a4a05d02a5cc54b7d709dc5f9d1e89
LittleOrange23 <43208783+LittleOrange23@users.noreply.github.com>
2019-03-14 16:37:12 +0800

    添加后台

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <script>
-export default {
-  created () {
-    // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+// import config from './config.js'
+import { get } from "./utils";
+import qcloud from "wafer2-client-sdk";
+import config from "./config";
 
-    console.log('app created and cache logs by setStorageSync')
+export default {
+  async created() {
+    // 设置登录地址
+    qcloud.setLoginUrl(config.loginUrl);
+    qcloud.login({
+      success: function(userInfo) {
+        console.log("登录成功", userInfo);
+      },
+      fail: function(err) {
+        console.log("登录失败", err);
+      }
+    });
+
+    // 调用API从本地缓存中获取数据
+    // const logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+
+    // const res = await get('/weapp/demo')
+    // console.log(res)
+    // wx.request({
+    //   url: config.host + '/weapp/demo',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
+    console.log("app created and cache logs by setStorageSync");
+    console.log("小程序启动了");
   }
-}
+};
 </script>
 
 <style>
--- src/pages/goodslist/index.vue
+++ src/pages/goodslist/index.vue
@@ -4,25 +4,25 @@
       <van-tab title="图书文学">
         <div>
           <goodsort></goodsort>
-          <goodslist></goodslist>
+          <goodslist :location="location"></goodslist>
         </div>
       </van-tab>
       <van-tab title="数码电子">
         <div>
           <goodsort></goodsort>
-          <goodslist></goodslist>
+          <goodslist :location="location"></goodslist>
         </div>
       </van-tab>
       <van-tab title="生活用品">
         <div>
           <goodsort></goodsort>
-          <goodslist></goodslist>
+          <goodslist :location="location"></goodslist>
         </div>
       </van-tab>
       <van-tab title="其他更多">
         <div>
           <goodsort></goodsort>
-          <goodslist></goodslist>
+          <goodslist :location="location"></goodslist>
         </div>
       </van-tab>
     </van-tabs>
@@ -41,12 +41,37 @@ export default {
 
   data() {
     return {
-
+      location: null
     }
   },
-
+  methods: {
+    getGeo() {
+      wx.getLocation({
+        type: "wgs84",
+        success: geo => {
+          console.log(geo);
+          wx.request({
+            url: `http://api.map.baidu.com/geocoder/v2/?location=${
+              geo.latitude
+            },${
+              geo.longitude
+            }&output=json&pois=1&ak=UoZlCMqkPaaxVeIVA30e35dVTmyGB26j`,
+            success: res => {
+              console.log(res);
+              console.log(res.data.result);
+              if (res.data.status === 0) {
+                this.location = res.data.result.formatted_address;
+              } else {
+                this.location = "未知地点";
+              }
+            }
+          });
+        }
+      });
+    }
+  },
   created() {
-
+    this.getGeo()
   }
 }
 </script>
